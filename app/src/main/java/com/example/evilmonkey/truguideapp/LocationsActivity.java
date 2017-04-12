c490e502be1347793be65c6449a291f9ff3bc5d5
Supparurk Jittarmars <supparurk1992@gmail.com>
2017-04-13 04:37:17 +0700

    เพิ่มฟังก์ชั่นการคลิก Marker
    เพิ่มฟังก์ชั่นการคลิกที่ Marker เพื่อเตรียมข้อมูลไว้ทำการ ส่งไปอีก
    Activity

--- app/src/main/java/com/example/evilmonkey/truguideapp/LocationsActivity.java
+++ app/src/main/java/com/example/evilmonkey/truguideapp/LocationsActivity.java
@@ -16,16 +16,20 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class LocationsActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mGoogleMap;
     private static final LatLngBounds TRUBound = new LatLngBounds(new LatLng(13.715627, 100.345581), new LatLng(13.717476, 100.350287));
     private static final LatLng TRUCenter = new LatLng(13.716375, 100.348672);
     private static final LatLng TRU_VIEW = new LatLng(13.716291, 100.347623);
 
-    private String[] Location_Name,Location_Icon,Location_Lat,Location_Lon,Location_Subtitle,Location_Detail;
+    private String[] Location_Name,Location_Icon,Location_Lat,Location_Lon,Location_Subtitle;
+
+    private Marker marker;
+
 
 
     @Override
@@ -38,8 +42,6 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
         Location_Lat = getResources().getStringArray(R.array.Location_Lat);
         Location_Lon = getResources().getStringArray(R.array.Location_Lon);
         Location_Subtitle = getResources().getStringArray(R.array.Location_Subtitle);
-        Location_Detail = getResources().getStringArray(R.array.Location_Detail);
-
 
         if(playServiceCheck()){
             Toast.makeText(this, "Welcome To Location View.", Toast.LENGTH_LONG).show();
@@ -70,23 +72,37 @@ public class LocationsActivity extends AppCompatActivity implements OnMapReadyCa
         return false;
 
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        //Setting Map
         int maxLocation = Location_Name.length;
         mGoogleMap = googleMap;
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         CameraPosition cameraPosition = new CameraPosition.Builder().target(TRU_VIEW).zoom(18).bearing(65).tilt(50).build();
         mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+        // Cerate Marker
         for(int i=0;i<=maxLocation-1;i++){
             Double lat = Double.parseDouble(Location_Lat[i]);
             Double lon = Double.parseDouble(Location_Lon[i]);
             LatLng location = new LatLng(lat,lon);
             //googleMap.addMarker(new MarkerOptions().position(location).title(Location_Name[i]).snippet(Location_Subtitle[i]).icon(BitmapDescriptorFactory.fromResource(R.drawable.mainlogo)));
-            googleMap.addMarker(new MarkerOptions().position(location).title(Location_Name[i]).snippet(Location_Subtitle[i]));
+            marker = googleMap.addMarker(new MarkerOptions().position(location).title(Location_Name[i]).snippet(Location_Subtitle[i]));
+            marker.setTag(i);
         }
+
+        //To Click for Event
+        mGoogleMap.setOnInfoWindowClickListener(this);
+
+    }
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        //Click To Inten New Windows
+        int position =  (int)(marker.getTag());
+        String title = Location_Name[position];
+
+        Toast.makeText(this, "แสดงข้อมูลของ "+title, Toast.LENGTH_SHORT).show();
+
 
     }
 }
